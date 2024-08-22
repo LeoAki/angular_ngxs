fe17ec69a920e5a324e4caa7d575ea292ff491eb
Leonardo Aquino <laquino@soaint.com>
2024-08-21 20:58:54 -0500

    Feat: Se agrega configuración inicial del componente App.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './core/components/nav-bar/nav-bar.component';
+import { FooterComponent } from './core/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavBarComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
