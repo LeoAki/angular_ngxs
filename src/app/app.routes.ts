a879c1ac81706660bd46f531500a8db0e1a453ca
Leonardo Aquino <laquino@soaint.com>
2024-08-21 20:57:22 -0500

    Chore: Se agrega configuración de rutas.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('../app/features/home/home.component').then((c) => c.HomeComponent)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
