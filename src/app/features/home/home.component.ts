4ef92b3c1544912816c60f441939139451a17cd7
Leonardo Aquino <laquino@soaint.com>
2024-08-28 18:14:54 -0500

    Feature: Se agregan las funcionalidades del NgRx a los componentes de la Aplicación.

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -3,6 +3,10 @@ import { Todo } from '../../core/interfaces/todo.interface';
 import { MatDialog } from '@angular/material/dialog';
 import { ModalTodoComponent } from '../../shared/components/modal-todo/modal-todo.component';
 import { TodoItemComponent } from '../../shared/components/todo-item/todo-item.component';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { createTodoRequest, getTodosRequest } from '../../store/actions/todo.actions';
+import { selectTodosDone, selectTodosInProgress, selectTodosPending } from '../../store/selectores/todo.selectors';
 
 @Component({
   selector: 'app-home',
@@ -12,13 +16,19 @@ import { TodoItemComponent } from '../../shared/components/todo-item/todo-item.c
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  tasksCreated: Todo[] = []
-  tasksInProgress: Todo[] = []
-  tasksCompleted: Todo[] = []
+  tasksCreated: Observable<Todo[]> = new Observable()
+  tasksInProgress: Observable<Todo[]> = new Observable()
+  tasksCompleted: Observable<Todo[]> = new Observable()
 
   readonly dialog = inject(MatDialog);
+  store = inject(Store<Store>);
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.store.dispatch(getTodosRequest()) // llama a la acción para obtener los todos
+    this.tasksCreated = this.store.select(selectTodosPending) // obtiene los todos pendientes
+    this.tasksInProgress = this.store.select(selectTodosInProgress) // obtiene los todos en progreso
+    this.tasksCompleted = this.store.select(selectTodosDone) // obtiene los todos completados
+  }
 
   createTodo() {
     const dialogRef = this.dialog.open(ModalTodoComponent, {
@@ -30,7 +40,7 @@ export class HomeComponent {
 
     dialogRef.afterClosed().subscribe(result => {
       if (!result) return
-
+      this.store.dispatch(createTodoRequest({todo: result}));
     })
   }
 }
--- src/app/shared/components/todo-item/todo-item.component.ts
+++ src/app/shared/components/todo-item/todo-item.component.ts
@@ -1,10 +1,13 @@
-import { ChangeDetectionStrategy, Component, inject, model } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, Input, model, signal } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Todo } from '../../../core/interfaces/todo.interface';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { ButtonStatusComponent } from '../button-status/button-status.component';
 import { MatIconModule } from '@angular/material/icon';
 import { ModalTodoComponent } from '../modal-todo/modal-todo.component';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { updateTodoRequest } from '../../../store/actions/todo.actions';
 
 @Component({
   selector: 'app-todo-item',
@@ -12,7 +15,8 @@ import { ModalTodoComponent } from '../modal-todo/modal-todo.component';
   imports: [
     CommonModule,
     ButtonStatusComponent,
-    MatIconModule
+    MatIconModule,
+    AsyncPipe
   ],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.css',
@@ -20,9 +24,10 @@ import { ModalTodoComponent } from '../modal-todo/modal-todo.component';
 })
 export class TodoItemComponent {
 
-  tasks = model<any[]>();
+  @Input() tasks: Observable<Todo[]> = new Observable()
   title = model<string>();
   readonly dialog = inject(MatDialog);
+  store = inject(Store<Store>);
 
   editTodoModal(todo: Todo) {
     const dialogRef = this.dialog.open(ModalTodoComponent, {
@@ -57,6 +62,8 @@ export class TodoItemComponent {
     });
   }
 
-  updateStatus(nuevoEstado: number, id: string) {}
+  updateStatus(nuevoEstado: number, id: string) {
+    this.store.dispatch(updateTodoRequest({ _id: id, todo: { statusId: nuevoEstado } }))
+  }
 
 }
