9b3d2fb2773a14d8c0e30bfbb082c3fc3f74ca8f
Leonardo Aquino <laquino@soaint.com>
2024-08-28 18:14:14 -0500

    Chore: Se agrega para su uso, la conf de NgRx.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideStore } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideEffects } from '@ngrx/effects';
+import { TodosEffects } from './store/effects/todo.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(), provideAnimationsAsync()
-  ]
+    provideHttpClient(), provideAnimationsAsync(),
+    provideStore(reducers, { metaReducers }),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    provideEffects([TodosEffects])
+]
 };
